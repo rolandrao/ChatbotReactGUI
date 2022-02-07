1500a01dd7b97f4b99abe879f1f68bc7c6b4569d
rolandrao <rolandrao@gmail.com>
2022-02-07 16:47:29 -0500

    fix for build router issues

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import InteractionWindow from "./InteractionWindow";
 import InstructorDashboard from "./InstructorDashboard";
 import CustomNavbar from "./CustomNavbar";
